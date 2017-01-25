f4d3066e52c19805bcc403df7df8914510c8ab15
luisaguilarcascante <aguilarc1992@gmail.com>
2017-01-25 09:04:08 -0600

    ¨AvanceTarea2¨

---
+++ B00092/T2_B00092/Tarea 2 (Temp Base)/polinomio.cpp
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   polinomio.cpp
+ * Author: adrian
+ * 
+ * Created on January 24, 2017, 9:31 PM
+ */
+
+#include "polinomio.hpp"
+#include <iostream>
+
+using namespace std;
+
+int polinomio::suma()
+{}
+int polinomio::resta()
+{}
+int polinomio::division()
+{}
+int polinomio::multiplicacion()
+{}
+
+
+void polinomio::resultado()
+{
+      cout << "Resultado" << this <<", ";
+	cout << polinomio <<", ";
+	cout << endl;
+	return;
+}
+
+polinomio::polinomio() {
+    
+}
+
+polinomio::polinomio(const polinomio& orig) {
+}
+
+polinomio::~polinomio() {
+}
+

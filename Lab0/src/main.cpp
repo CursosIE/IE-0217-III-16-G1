ea7f13521bec0be3dea075a03dc28ff819a29b25
giancamarin <gianca.1996mh@gmail.com>
2017-01-28 03:05:46 -0600

    Correcciones: main.cpp forma de creacion del tablero de juego y lectura de datos del .txt

--- Lab0/src/main.cpp
+++ Lab0/src/main.cpp
@@ -19,57 +19,59 @@
  * @param int 		Cantidad de parametros obtenidos en la ejecucion del programa
  * @param char**	Vector de char* con los parametros enviados en la ejecucion
 */ 
-int main (int argc, char** argv){
-	//variables necesarias
-	int filas, columnas,dias;
+int main(int argc, char *argv[]){
+	int filas, columnas, dias;
 	short int zacate;
-	bool feMale;
+	bool genero;
 	char animal, sexo;
-	int proba[filas][columnas];
-	dias = atoi(argv[1]);//tomar cantidad de dias como primer argumento al correr el programa
-	ifstream file(argv[2]);//lectura del archivo de texto como segundo argumento al correr el programa
+	dias = atoi(argv[1]);
+	//lectura del archivo de configuracion del juego
+	ifstream file(argv[2]); 
 	string str,temp;
-	//asignaciones
 	getline(file, str);
 	filas = atoi(str.c_str());//.c_srt() método para convertir string a vector de caracteres
 	getline(file, str);
 	columnas = atoi(str.c_str());
 	//inicializacion de la matriz de objetos tipo celda
 	Celda** tablero = new Celda*[filas];
-	
-	for(int f = 0; f < filas;f++ ){
-		tablero[f] = new Celda[columnas];
+	for(int i=0; i<filas;i++ ){
+		tablero[i] = new Celda[columnas];
 	}
 	while (getline(file, str))//Mientras se encuentre texto al leer el archivo guardar cada linea en el string str
 	{
-		stringstream test(str);//construir un objeto stringstream ya que se necesita para leer de el con la funcion getline;
-		getline(test, temp,'\t');//separa el stringstream cada caracter \t y fuardarlo en el string temp
-		filas = atoi(temp.c_str());//atoi convierte en int el vector de caracteres retornado por .c_str()
+		stringstream test(str);
+		//separa el stringstream cada caracter por un tab y lo guarda en temp
+		getline(test, temp,'\t');
+		filas = atoi(temp.c_str());
 		getline(test, temp,'\t');
 		columnas = atoi(temp.c_str());
 		getline(test, temp,'\t');
 		zacate = atoi(temp.c_str());
 		getline(test, temp,'\n');
-		if(temp != "\0"){//si el string no está vacio
-			animal = temp[0];//el primer caracter es el tipo de animal
-			sexo = (int) temp[1];//el segundo es el sexo
-			if(sexo == 49 ){//el char '1' tiene el valor entero:  49
-				feMale = 1;
+		if(temp != "\0"){
+			//si existe animal
+			animal = temp[0];
+			sexo = (int) temp[1];
+			if(sexo == 72){
+				//el char 'H' tiene el valor entero:  72 que indica genero femenino (TRUE)
+				genero = 1;
 			}
 			else{
-			feMale = 0;
+				genero = 0;
 			}
-		}	
-		proba[filas][columnas] = filas + columnas;
-		cout << str << endl;
-		cout << filas << endl;
-		cout << columnas << endl;
-		cout << zacate << endl;
-		cout << animal << endl;
-		cout << sexo << endl;
-		cout << feMale << endl;
+		}
+		else{
+			//caso en que no haya un animal en una celda
+			animal='x';
+			genero=0;
+		}
+		//Asignacion de informacion a cada celda
+		Celda* nuevaCelda= new Celda(columnas, filas, zacate,  tablero, animal, sexo);
+		tablero[filas][columnas]=*nuevaCelda;
+		cout << filas  << "\t" <<  columnas <<  "\t" << zacate << "\t" << 
+		animal << sexo << endl;
 	}
-	cout << "Cantidad de dias: " << dias << endl;
-
+	cout << "Dias que no faltan de transcurrir: " << dias << endl;
+	dias--;
 	return 0;
 }

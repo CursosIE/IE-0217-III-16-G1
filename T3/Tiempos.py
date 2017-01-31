aa7eb7d096b6debce49e09eb6345f8a20ea1c8c5
luisaguilarcascante <aguilarc1992@gmail.com>
2017-01-31 14:32:03 -0600

    ¨CodigoPythonQueCalculaTiemposDeEjecución¨

---
+++ T3/Tiempos.py
@@ -0,0 +1,15 @@
+from math import sin
+
+from time import time
+
+
+inicial = time() 
+
+/* algoritmo */ 
+
+
+final = time() 
+ 
+ejecucion = final - inicial
+
+print 'Tiempo de ejecucion igual a:', ejecucion
